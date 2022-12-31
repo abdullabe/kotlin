8fdd18e5f587e3b0dcb56a45a3cde62bf7375808
DELL <abdullaabdulla422@gmail.com>
2022-12-31 12:49:36 +0530

    initial commit

---
+++ app/src/main/java/com/project/kotlin/ItemsViewModel.kt
@@ -0,0 +1,3 @@
+package com.project.kotlin
+
+data class ItemsViewModel(val image: Int, val text: String)
---
+++ app/src/main/java/com/project/kotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.project.kotlin
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
